5a1fc05ffff403548e9aa1edc6efd186bd1fe01a
srutibaliga <sruti.baliga@gmail.com>
2026-06-01 21:17:54 +0530

    fix: add nodemailer network connection timeouts to prevent API hanging

--- server/index.js
+++ server/index.js
@@ -22,7 +22,10 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
+  connectionTimeout: 10000, // 10 seconds connection timeout
+  greetingTimeout: 10000,
+  socketTimeout: 10000
 });
 
 const app = express();
